1aa211ef9c200a61a8d4c9c032de380947367961
Dmitry Povyaznikov <d.podvyaznikov@gmail.com>
2019-01-16 15:42:51 +0300

    Add script for mask transform

---
+++ deepglobe/prepare_masks.py
@@ -0,0 +1,47 @@
+""" Prepare DeepGlobe train masks """
+
+# Renames mask from 'train' folder to the same name as image, but with
+# '.png' extension. Also transforms mask from H*W*3 format (last axis has
+# values 0 and 255, combinations of 0 and 255 encode classes) to H*W*1 format, where last axis
+# has class number values from  0 to 6.
+
+import os
+import argparse
+import numpy as np
+from multiprocessing import Pool
+from PIL import Image
+
+def transform_mask_to_class_numbers(fname):
+    """
+    """
+    mask = np.array(Image.open(fname))
+    mask = mask // 255 # to 0 / 1 values
+    base = 2 ** np.arange(3)[::-1] # binary base
+    mask = (mask * base).sum(axis=-1).astype(np.uint8)
+    Image.fromarray(mask).save(fname)
+
+def main():
+    """
+    """
+    parser = argparse.ArgumentParser('')
+    parser.add_argument('--path', dest='path', default=None,
+                        help='Path to the files')
+    args = parser.parse_args()
+    path = args.path
+    if not path:
+         raise ValueError('You need to specify path to the files with `--path`.')
+
+    names = os.listdir(path)
+    mask_names = [os.path.join(path, name) for name in names if name.endswith('png')]
+
+
+    pool = Pool(4)
+
+    results = pool.map(transform_mask_to_class_numbers, mask_names)
+
+    #close the pool and wait for the work to finish 
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    main()
